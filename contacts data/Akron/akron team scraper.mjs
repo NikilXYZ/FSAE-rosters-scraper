3dfafcb66fbea2776235bdcfdbd619f349b3955f
NikilXYZ <nikil@inventXYZ.com>
2021-05-23 01:15:32 -0500

    akron zips teams

---
+++ contacts data/Akron/akron team scraper.mjs
@@ -0,0 +1,33 @@
+// this works for both their electric 20 team as well as combustion 20 team
+
+// this is a squarespace site
+let text_elements = Array.from(document.querySelectorAll(".sqs-block-html .sqs-block-content"))
+
+// ignore the section headers
+
+// people's information are only the blocks that start with an h3 and have at least one p
+let ppl_elements = text_elements.filter(element => {
+    try {
+        return element.children[0].matches("h3") && element.children[1].matches("p")
+    } catch (error) {
+        return false
+    }
+})
+
+let ppl = ppl_elements.map(person_element => {
+    let person = {
+        "role": person_element.children[1].innerText,
+        "name": person_element.children[0].textContent
+        // the CSS has a text-transform that makes the names all caps.
+        // innerText shows up as all caps, but textContent has the raw text
+        // of course, if they typed the name weirdly to begin with, just have to manually fix those names
+    }
+    let email = undefined
+    try { 
+        email = person_element.children[2].innerText
+    } catch (error) {}
+    if (email) person.email = email
+    return person
+})
+
+copy(ppl)
