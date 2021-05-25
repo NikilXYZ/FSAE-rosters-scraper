28742a78b79fb2b03784b2214ded42fcb8ec3d4f
NikilXYZ <nikil@inventXYZ.com>
2021-05-24 22:23:10 -0500

    added minnesota

---
+++ contacts data/Minnesota/minnesota 2020 team scraper.mjs
@@ -0,0 +1,24 @@
+// the 2020 team leads are listed below, so I'm just looking at that list
+// if I wanted 2021 team, then scraping would make more sense. Maybe for next year's hires!
+
+let team_containers = document.querySelectorAll(".gtr-uniform")
+
+let current_team = team_containers[0].children[0].children[0].children
+let past_teams = team_containers[1].children
+
+let just_finished_team = Array.from(past_teams[0].children[0].children)
+
+let just_finished_team_year = parseInt(just_finished_team[0].innerText, 10)
+
+let just_finished_team_members = just_finished_team.slice(1)
+
+let ppl_nested_arrays = just_finished_team_members.map(person_element => person_element.innerText.replace(/[\s]+/g, " ").split(": "))
+
+ppl = ppl_nested_arrays.map(person_nested_array => ({
+    "school": "University of Minnesota",
+    "team type": "Combustion",
+    "role" : person_nested_array[0],
+    "name": person_nested_array[1]
+}))
+
+copy(ppl)
