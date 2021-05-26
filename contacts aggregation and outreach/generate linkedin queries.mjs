99fc2644ed36b4568f3715e190a0868de78de74f
NikilXYZ <nikil@inventXYZ.com>
2021-05-25 21:54:50 -0500

    linkedin queries

--- contacts aggregation and outreach/generate linkedin queries.mjs
+++ contacts aggregation and outreach/generate linkedin queries.mjs
@@ -2,3 +2,16 @@ import fs from "fs"
 
 let students = JSON.parse(fs.readFileSync("./all fsae students.json"))
 
+let linkedin_query_generator = (queries_so_far, student) => {
+    if (!student.linkedin) {
+        let linkedin_query = encodeURI(`https://www.linkedin.com/search/results/all/?keywords=${student.name} ${student.university}`)
+        queries_so_far.push(linkedin_query)
+    }
+    return queries_so_far
+}
+
+let linkedin_queries = students.reduce(linkedin_query_generator, [])
+
+fs.writeFileSync("all linkedin queries.json", JSON.stringify(linkedin_queries))
+
+let x = 2
